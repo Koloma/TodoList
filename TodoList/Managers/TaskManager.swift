12776bca4b2d0ec5cc60c3ad0692ae9200e66fe9
Alexandr Kolomenskiy <koloma2@mail.ru>
2023-02-12 23:40:24 +0700

    add description quickHelp for TaskManager

--- TodoList/Managers/TaskManager.swift
+++ TodoList/Managers/TaskManager.swift
@@ -15,6 +15,7 @@ protocol ITaskManager {
 	func delTask (task: ITask)
 }
 
+///Менеджер по управлению списком задач
 final class TaskManager: ITaskManager {
 
 	private var tasks:[ITask] = []
@@ -23,28 +24,35 @@ final class TaskManager: ITaskManager {
 		self.tasks = tasks
 	}
 
+	///Возвращает список всех задач
 	func allTasks() -> [ITask] {
 		return tasks
 	}
 
+	///Возвращает список завершенных задач
 	func completedTasks() -> [ITask] {
 		return tasks.filter { $0.completed }
 	}
 
+	///Возвращает список не завершенных задач
 	func notCompletedTasks() -> [ITask] {
 		return tasks.filter { !$0.completed }
 	}
 
+	///Добавляет новую задачу к списку задач
 	func addTask(task: ITask) {
 		tasks.append(task)
 	}
 
+	//Удаляет задачу из списка задач
 	func delTask (task: ITask) {
 		tasks.removeAll { $0.title == task.title }
 	}
 }
 
 extension TaskManager {
+
+	///Возвращает список задач заглушек для демонстрации
 	static func CreateMockTaskList() -> [ITask] {
 		let task = RegularTask(title: "RegTask 2")
 			task.setCompleted(true)
