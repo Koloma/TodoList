20fd7a7350dce5429502ca0444396b854cff084d
Alexandr Kolomenskiy <koloma2@mail.ru>
2023-02-12 15:00:56 +0700

    add task table view cells

---
+++ TodoList/TableCells/RegularTaskTableViewCell/RegularTaskTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RegularTaskTableViewCell.swift
+//  TodoList
+//
+//  Created by Коломенский Александр on 12.02.2023.
+//
+
+import UIKit
+
+class RegularTaskTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
