f5402b880cc080ccdd9e51938fbd76b9f15c521a
Alexandr Kolomenskiy <koloma2@mail.ru>
2023-02-15 20:39:27 +0700

    del bad code

--- TodoList/Flows/MainScreen/MainScreenViewController.swift
+++ TodoList/Flows/MainScreen/MainScreenViewController.swift
@@ -10,50 +10,18 @@ import UIKit
 class MainScreenViewController: UIViewController {
 
 	private let tableView: UITableView = UITableView()
-	private var taskManager: ITaskManager? = nil
-	private var taskList: [ITask] = []
-	private var currentTaskListStates: TaskListState = .all {
-		didSet {
-			tableReloadData()
-		}
-	}
+	private var taskManager: ITaskManager
 
-	private func tableReloadData() {
-		switch currentTaskListStates {
-		case .all:
-			taskList = taskManager?.allTasks() ?? []
-		case .notCompleted:
-			taskList = taskManager?.notCompletedTasks() ?? []
-		case .completed:
-			taskList = taskManager?.completedTasks() ?? []
-		}
-		tableView.reloadData()
-	}
-
-	private enum TaskListState: String, CaseIterable {
-		case all = "All"
-		case notCompleted = "Not completed"
-		case completed = "Completed"
-	}
-	private let segmentedControl: UISegmentedControl = {
-
-		let items = TaskListState.allCases.map { $0.rawValue }
-		let segmentControl = UISegmentedControl(items: items)
-		segmentControl.selectedSegmentIndex = 0
-		return segmentControl
-	}()
-
+	// MARK: init
 	init(taskManager: ITaskManager) {
-
 		self.taskManager = taskManager
-		self.taskList = taskManager.allTasks()
-
 		super.init(nibName: nil, bundle: nil)
 	}
-	
+
 	required init?(coder: NSCoder) {
-		super.init(coder: coder)
+		fatalError("init(coder:) has not been implemented")
 	}
+
 	
 	// MARK: viewDidLoad
 	override func viewDidLoad() {
@@ -62,6 +30,7 @@ class MainScreenViewController: UIViewController {
 		initTableView()
 	}
 
+	// MARK: initViewTableView
 	private func initTableView() {
 		tableView.dataSource = self
 		tableView.register(
@@ -72,58 +41,41 @@ class MainScreenViewController: UIViewController {
 			, forCellReuseIdentifier: ImportantTaskTableViewCell.reuseCellID)
 
 	}
+
+	// MARK: initView
 	private func initView() {
-		view.backgroundColor = .systemBackground
 
-		segmentedControl.addTarget(self, action: #selector(segmentedControlValueChanged(_:)), for: .valueChanged)
-		view.addSubview(segmentedControl)
-//		segmentedControl.snp.makeConstraints { make in
-//			make.left.right.equalTo(view)
-//			make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top)
-//
-//		}
 		view.addSubview(tableView)
-//		tableView.snp.makeConstraints { make in
-//			make.top.equalTo(segmentedControl.snp.bottom).offset(15)
-//			make.left.right.bottom.equalTo(view)
-//		}
+		tableView.translatesAutoresizingMaskIntoConstraints = false
+		tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+		tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+		tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+		tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+
 	}
 
-	@objc func segmentedControlValueChanged(_ sender: UISegmentedControl) {
-		switch sender.selectedSegmentIndex {
-		case 0:
-			currentTaskListStates = .all
-		case 1:
-			currentTaskListStates = .notCompleted
-		case 2:
-			currentTaskListStates = .completed
-		default:
-			taskList = []
-			currentTaskListStates = .all
-		}
-	}
 
 }
 
+// MARK: extension MainScreenViewController
 extension MainScreenViewController: UITableViewDataSource, UITableViewDelegate {
 
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return taskList.count
+		return taskManager.allTasks().count
 	}
 
 	func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-		switch taskList[indexPath.row] {
-		case is RegularTask:
-			return RegularTaskTableViewCell.cellHeight
-		case is ImportantTask:
-			return ImportantTaskTableViewCell.cellHeight
-		default :
-			return 50
-		}
+//		switch taskList[indexPath.row] {
+//		case is RegularTask:
+//			return RegularTaskTableViewCell.cellHeight
+//		case is ImportantTask:
+//			return ImportantTaskTableViewCell.cellHeight
+//		default :
+			return 80
 	}
 
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-		switch taskList[indexPath.row] {
+		switch taskManager.allTasks()[indexPath.row] {
 		case let task where task is RegularTask:
 			guard let cell = tableView.dequeueReusableCell(
 				withIdentifier: RegularTaskTableViewCell.reuseCellID
@@ -133,10 +85,8 @@ extension MainScreenViewController: UITableViewDataSource, UITableViewDelegate {
 				cell.config(
 						model: RegularTaskCellModelInput(
 						task: task as! RegularTask)
-						,modelOutput: { [weak self] value in
-							guard let self = self else { return }
+						,modelOutput: { value in
 							task.setCompleted(value)
-							self.tableReloadData()
 						}
 				)
 				return cell
@@ -150,10 +100,8 @@ extension MainScreenViewController: UITableViewDataSource, UITableViewDelegate {
 				cell.config(
 						model: ImportantTaskCellModelInput(
 						task: task as! ImportantTask)
-						,modelOutput: { [weak self] value in
-							guard let self = self else { return }
+						,modelOutput: { value in
 							task.setCompleted(value)
-							self.tableReloadData()
 						}
 				)
 				return cell
